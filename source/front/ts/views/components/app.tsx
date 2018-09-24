54259e89e5ba9612709b169e927152ca4b0d1ed6
ConstantineGite <apostull@rambler.ru>
2018-09-24 17:43:16 +0300

    chore(app.tsx && header/index.tsx) add component header

---
+++ source/front/ts/views/components/app.tsx
@@ -0,0 +1,18 @@
+
+import { Component, createElement } from "react";
+import Component from "/react";
+
+export class App extends Component {
+	public render(): JSX.Element {
+		return (
+			<p>sdfsdf</p>
+		);
+	}
+
+	// private collectLists(): void {
+
+	// }
+
+	// private getList(options: any): JSX.Element {
+
+	// }
---
+++ source/front/ts/views/components/header/index.tsx
@@ -0,0 +1,8 @@
+import { Component, createElement } from "react";
+
+export class Header extends Component {
+
+	public render(): JSX.Element {
+		return  createElement("div", null, "dfsdfsdfsdf");
+	}
+}
