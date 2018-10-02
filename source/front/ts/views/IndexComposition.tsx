5c56bb992c0e8adc344b2758c855848d830148e9
ConstantineGite <apostull@rambler.ru>
2018-10-02 15:24:47 +0300

    chore(IndexComposition.tsx)add midleBlock

--- source/front/ts/views/IndexComposition.tsx
+++ source/front/ts/views/IndexComposition.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
+import App from "./components/app";
 
 export class IndexComposition extends React.Component {
 	public render(): JSX.Element {
 		return (
 			<BrowserRouter>
-				<div>awdawd</div>
+				<App />
 			</BrowserRouter>
 		);
 	}
