0b3743bc38f575df900d850a9497b21faf1ca830
ConstantineGite <apostull@rambler.ru>
2018-10-31 18:00:57 +0200

    corect style

--- source/front/ts/views/components/midle_block/components/slider/index.tsx
+++ source/front/ts/views/components/midle_block/components/slider/index.tsx
@@ -1,11 +1,68 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import * as React from "react";
+	// import _Config from "~/c/config";
+	// import { Data } from "~/c/data";
+	// import { Greeter } from "~/c/timeTriger";
+	// import { Informer } from "../common/informer";
+let numberSlide = 0;
 
-export default class SliderBlock extends Component<{}> {
+interface IState {
+	slidePosition: 0;
+	nameElem: "fadein";
+}
 
-	public render(): JSX.Element | null {
+export default class SliderBlock extends React.Component {
+	public state: IState = {
+		slidePosition: 0,
+		nameElem: "fadein"
+	};
+
+	private isMount: boolean = false;
+
+	public render(): JSX.Element {
+		//this.nextSlide();
+		console.log(this.elementColection().props.children[2], "elementColection()");
 		return (
-			<div id="slder"></div>
+			<div>
+				<main className="fadein"></main>
+			</div>
 		);
 	}
+
+	public componentDidMount(): void {
+		// this.isMount = true;
+
+		// Greeter.subscribe(
+		// 	(): void => {
+		// 		if (!this.isMount) return;
+		// 		this.nextSlide();
+		// 	}
+		// );
+	}
+
+	// public componentWillUnmount(): void {
+	// 	this.isMount = false;
+	// }
+	private elementColection(): JSX.Element {
+		return (
+			<ul>
+				<li className="0">1</li>
+				<li className="1">2</li>
+				<li className="2">3</li>
+				<li className="3">4</li>
+			</ul>
+		);
+	}
+
+	private nextSlide(): number {
+		console.log("start module ---> ---> --->");
+		setInterval(() => {
+			numberSlide++;
+			console.log("TIMER START" + numberSlide);
+		}, 1000);
+		return 3;
+	}
+
+	// private rotation(): JSX.Element[] {
+
+	// }
 }
--- source/front/ts/views/components/midle_block/index.tsx
+++ source/front/ts/views/components/midle_block/index.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import SliderBlock from "./components/slider/index";
 
 export default class MidleBlock extends Component<{}> {
 
 	public render(): JSX.Element | null {
+		console.log("start module__-----__+++__+_____----->---->----->---------->");
 		return (
-			<main>фцвфцвфцвфцвфц</main>
+				<main>
+					<SliderBlock />
+				</main>
 		);
 	}
 
